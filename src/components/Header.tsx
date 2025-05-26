997e1366481e1de5cd13e6a71cdb6974824b04d8
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2025-05-26 21:18:43 +0530

    bkcd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,22 +8,25 @@ import Loader from "./Loader";
 const lavishlyYours = Lavishly_Yours({ subsets: ["latin"], weight: "400" });
 
 const Header: React.FC = () => {
-  const {isLoading, visitorCount} = useUniqueUserRegister();
-  
+  const { isLoading, visitorCount } = useUniqueUserRegister();
+
   return (
     <header className="relative flex items-center justify-center py-4 w-full border-b border-zinc-700 shadow-md">
       {/* Name in the center */}
-      <h3 className={`${lavishlyYours.className} text-2xl text-white tracking-wider drop-shadow-lg`}>
+      <h3
+        className={`${lavishlyYours.className} text-2xl text-white tracking-wider drop-shadow-lg`}
+      >
         Ravi Gangwar
       </h3>
 
       {/* Views aligned to the right */}
       <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center gap-1">
         <FaUserFriends color="white" size={18} />
-        {
-          isLoading ? <Loader/> :
+        {isLoading ? (
+          <Loader />
+        ) : (
           <p className="text-white text-center text-sm">{visitorCount}</p>
-          }
+        )}
       </div>
     </header>
   );
